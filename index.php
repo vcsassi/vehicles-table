4062f4df34e53ec830c05777aef6a7241017bc8e
vcsassi <vcsassi@gmail.com>
2015-08-30 20:01:29 -0500

    finish HTML basic tags

--- index.php
+++ index.php
@@ -1,33 +1,45 @@
-<?php
+
+<html>
+	<head>
+
+	</head>
+	<body>
 
 
-require("../../../dbconnection.inc.php");
 
-$conn = new mysqli($host, $db_user, $db_password, $db_name);
 
-if($conn->connect_error){
-	die("We could not connect" . $conn->connect_error);
-} 
+		<?php
 
-$theSnazzySql =  "SELECT * FROM vehicles 
-		  LEFT OUTER JOIN makers 
-		  ON makers.id = vehicles.make_id";
 
-$result = $conn->query($theSnazzySql);
+		require("dbconnection.inc.php");
 
-if($result->num_rows > 0){
-	while($row = $result->fetch_assoc()){
-		echo "<div>";
-		echo " Make: ". $row["name"];
-		echo " Model: ".$row["model"];
-		echo " Color: ".$row["color"];
-		echo " Year: ".$row["year"];
-		echo " Engine".$row["engine"];
+		$conn = new mysqli($host, $db_user, $db_password, $db_name);
 
-		echo "</div>";
-	}
-}
+		if($conn->connect_error){
+			die("We could not connect" . $conn->connect_error);
+		} 
 
-$conn->close();
+		$theSnazzySql =  "SELECT * FROM vehicles 
+				  LEFT OUTER JOIN makers 
+				  ON makers.id = vehicles.make_id";
 
-?>
+		$result = $conn->query($theSnazzySql);
+
+		if($result->num_rows > 0){
+			while($row = $result->fetch_assoc()){
+				echo "<div>";
+				echo " Make: ". $row["name"];
+				echo " Model: ".$row["model"];
+				echo " Color: ".$row["color"];
+				echo " Year: ".$row["year"];
+				echo " Engine".$row["engine"];
+
+				echo "</div>";
+			}
+		}
+
+		$conn->close();
+
+		?>
+	</body>
+</html>
