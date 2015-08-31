5e6b1c03e19d8683fce0ecb0d1fb946e5fcfd8e2
vcsassi <vcsassi@gmail.com>
2015-08-30 22:32:01 -0500

    table name and require path correction

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 
 <html>
 	<head>
+	<meta charset="utf-8">
 
 	</head>
 	<body>
@@ -11,28 +12,32 @@
 		<?php
 
 
-		require("dbconnection.inc.php");
+
+		require '../../dbconnection.inc.php';
 
 		$conn = new mysqli($host, $db_user, $db_password, $db_name);
 
 		if($conn->connect_error){
 			die("We could not connect" . $conn->connect_error);
+
 		} 
 
-		$theSnazzySql =  "SELECT * FROM vehicles 
+
+		$theSnazzySql =  "SELECT * FROM vehicle
 				  LEFT OUTER JOIN makers 
-				  ON makers.id = vehicles.make_id";
+				  ON makers.id = vehicle.make_id";
 
 		$result = $conn->query($theSnazzySql);
 
 		if($result->num_rows > 0){
 			while($row = $result->fetch_assoc()){
 				echo "<div>";
-				echo " Make: ". $row["name"];
-				echo " Model: ".$row["model"];
-				echo " Color: ".$row["color"];
+				echo " Make: ". $row["make_id"];
+				echo " Model: ".$row["type"];
+				echo " Engine: ".$row["engine"];
 				echo " Year: ".$row["year"];
-				echo " Engine".$row["engine"];
+				echo " Fuel".$row["fuel"];
+				echo " Model".$row["model"];
 
 				echo "</div>";
 			}
