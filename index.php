b26e5a82abe2454523e0a80223ae5d91d3a0c173
vcsassi <vcsassi@gmail.com>
2015-09-03 11:58:44 -0500

    bootstrap functionality

--- index.php
+++ index.php
@@ -2,49 +2,29 @@
 <html>
 	<head>
 	<meta charset="utf-8">
-
 	</head>
 	<body>
-
-
-
-
 		<?php
 
+			require('dvconnect.inc.php');
+			
+			$conn = new mysqli($host, $db_user, $db_password, $dbname);
+			
+			
+			if($conn->connect_error){
 
+				die("We could not connect" . $conn->connect_error);
+			} 
+			$theSnazzySql =  "SELECT * FROM vehicles LEFT OUTER JOIN makers ON makers.id = vehicles.make_id";
+			$result = $conn->query($theSnazzySql);
+			if($result->num_rows > 0){
+				while($row = $result->fetch_assoc()){
 
-		require '../../dbconnection.inc.php';
-
-		$conn = new mysqli($host, $db_user, $db_password, $db_name);
-
-		if($conn->connect_error){
-			die("We could not connect" . $conn->connect_error);
-
-		} 
-
-
-		$theSnazzySql =  "SELECT vehicle.*, makers.name FROM vehicle
-				  LEFT OUTER JOIN makers
-				  ON makers.id = vehicle.make_id";
-
-		$result = $conn->query($theSnazzySql);
-
-		if($result->num_rows > 0){
-			while($row = $result->fetch_assoc()){
-				echo "<div>";
-				echo " Make: ". $row["name"];
-				echo " Model: ".$row["type"];
-				echo " Engine: ".$row["engine"];
-				echo " Year: ".$row["year"];
-				echo " Fuel".$row["fuel"];
-				echo " Model".$row["model"];
-
-				echo "</div>";
+					echo " Make: ". $row["id"]; . " Type: " .$row["type"]; . " Engine: ".$row["engine"]; . " Year: ".$row["year"]; ." Fuel: ".$row["fuel"] . " Model: ".$row["model"] . "</br>";
+				}
 			}
-		}
-
-		$conn->close();
-
+			$conn->close();
 		?>
+
 	</body>
 </html>
